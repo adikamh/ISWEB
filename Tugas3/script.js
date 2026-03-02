e9d9efa1a5cf079da0a80332edf724d82d1f92cc
adikamh <haikaladika272@gmail.com>
2026-03-02 10:27:20 +0700

    Lupa tampabih komentar

--- Tugas3/script.js
+++ Tugas3/script.js
@@ -1,4 +1,7 @@
-
+/*saya mengerjakan tugas ini sendiri tanpa melakukan kecurangan seperti menggunakan,
+  mendupliaksi kode perogram kode program yang ada. jia saya melakukan kecurangan dalam 
+  perngerjaan tugas ini maka saya siap menerima sanksi atau hukuman dari dari dosen mata 
+  kuliah dan tuhan / allah  swt*/
 
 const btnSimpan = document.getElementById('btnSimpan');
 const bgInput = document.getElementById('bgInput');
