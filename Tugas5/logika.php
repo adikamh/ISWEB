b65ce6c8a4105f52e086f8411baaf93e913d8bdb
adikamh <haikaladika272@gmail.com>
2026-03-11 12:24:01 +0700

    Benerin conflict

--- Tugas5/logika.php
+++ Tugas5/logika.php
@@ -1,9 +1,8 @@
 <?php
-        for ($i = 1; $i <= 5; $i++) {
-            echo "<tr>";
-            for ($j = 1; $j <= 5; $j++) {
-                echo "<td>$i,$j</td>";
-            }
-            echo "</tr>";
-        }
-        ?>
+for ($i = 1; $i <= 5; $i++) {
+    echo "<tr>";
+    for ($j = 1; $j <= 5; $j++) {
+        echo "<td>$i,$j</td>";
+    }
+    echo "</tr>";
+}
