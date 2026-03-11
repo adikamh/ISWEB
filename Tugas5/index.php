1b92841eaad89038147de0686649f5133340d0b0
adikamh <haikaladika272@gmail.com>
2026-03-11 12:11:59 +0700

    Tugas dikelas tentang php

---
+++ Tugas5/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Tabel PHP Dinamis</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <table>
+        <?php include 'logika.php'; ?>
+    </table>
+
+</body>
+</html>
